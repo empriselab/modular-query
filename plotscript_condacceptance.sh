6c0966a37a3593380c4e4ed7b11a0a76d07063e1
Rohan Banerjee <rohan.b.banerjee@gmail.com>
2025-12-11 17:39:52 -0500

    add lower set of plots to plot script

--- plotscript_condacceptance.sh
+++ plotscript_condacceptance.sh
@@ -1,4 +1,12 @@
-python experiments/plot_results_grid.py --results_dir experiments/results/20251208_hricondaccept/ --plot_variable 'num_modules' --output_dir experiments/results/20251208_hricondaccept/01_nummodules --pkl_file results_variant_balanced-2_run_02.pkl --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
+
+## Upper plots.
+# python experiments/plot_results_grid.py --results_dir experiments/results/20251208_hricondaccept/ --plot_variable 'num_modules' --output_dir experiments/results/20251208_hricondaccept/01_nummodules --pkl_file results_variant_balanced-2_run_02.pkl --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
 # python experiments/plot_results_grid.py --results_dir experiments/results/20251208_hricondaccept/ --plot_variable 'graph_structures' --output_dir experiments/results/20251208_hricondaccept/02_structures --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
 # python experiments/plot_results_grid.py --results_dir experiments/results/20251208_hricondaccept/ --plot_variable 'confidences' --output_dir experiments/results/20251208_hricondaccept/03_confidences --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
 # python experiments/plot_results_grid.py --results_dir experiments/results/20251208_hricondaccept/ --plot_variable 'query_costs' --output_dir experiments/results/20251208_hricondaccept/04_querycosts --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
+
+## Lower plots.
+python experiments/plot_results_grid.py --results_dir experiments/results/20250929_fixbruteforce/ --plot_variable 'num_modules' --output_dir experiments/results/20250929_fixbruteforce/01_nummodules --pkl_file results_variant_balanced-2_run_02.pkl --run_id '02' --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
+# python experiments/plot_results_grid.py --results_dir experiments/results/20250929_fixbruteforce/ --plot_variable 'graph_structures' --output_dir experiments/results/20250929_fixbruteforce/02_structures --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
+# python experiments/plot_results_grid.py --results_dir experiments/results/20250929_fixbruteforce_varyconfidences/ --plot_variable 'confidences' --output_dir experiments/results/20250929_fixbruteforce_varyconfidences/03_confidences --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
+# python experiments/plot_results_grid.py --results_dir experiments/results/20250929_fixbruteforce/ --plot_variable 'query_costs' --output_dir experiments/results/20250929_fixbruteforce/04_querycosts --fixed_variant balanced-2 --fixed_module_selector 'Graph Query'
